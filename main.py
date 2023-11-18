676568417c83296a7f1a2925c2285c78158cfbb3
youpiteron <oyushchenko@kse.org.ua>
2023-11-18 11:30:41 +0200

    Fixed cipher functions, added console ui

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-def encrypt_formula(symbol, key, first_letter):
-    return (symbol - ord(first_letter) + key) % 26 + ord(first_letter)
+import random
+
+
+def encrypt_formula(symbol: str, key: int, first_letter: str):
+    return chr((ord(symbol) - ord(first_letter) + key) % 26 + ord(first_letter))
 
 
 def decrypt_formula(symbol, key, first_letter):
-    return (symbol - ord(first_letter) + key) % 26 + ord(first_letter)
+    return chr(((ord(symbol) - key - ord(first_letter)) % 26 + 26) % 26 + ord(first_letter))
 
 
-def f_cipher(text, key, formula):
+def f_cipher(text: str, key: int, formula):
     output = ""
     for i in range(0, len(text)):
-        c = text[0]
+        c = text[i]
         if c.isupper():
             c = formula(c, key, 'A')
         elif c.islower():
@@ -18,10 +21,37 @@ def f_cipher(text, key, formula):
     return output
 
 
-def encrypt(text, key):
+def encrypt(text: str, key: int):
     return f_cipher(text, key, encrypt_formula)
 
 
-def decrypt(text, key):
+def decrypt(text: str, key: int):
     return f_cipher(text, key, decrypt_formula)
+
+
+def normal_mode():
+    inp = input("Enter text: ")
+    operation = input("Chose the operation (encrypt or decrypt): ")
+    key = int(input("Enter key: "))
+    if operation == "encrypt":
+        return encrypt(inp, key)
+    elif operation == "decrypt":
+        return decrypt(inp, key)
+
+
+def secret_mode():
+    inp = input("Enter text: ")
+    key = random.randint(1, 26)
+    return encrypt(inp, key)
+
+
+mode = input("Chose the mode (normal or secret): ")
+if mode == "normal":
+    print(normal_mode())
+elif mode == "secret":
+    print(secret_mode())
+
+
+
+
 
